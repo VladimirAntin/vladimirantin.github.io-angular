16dca3b5cf3759ae7e04a6669a7b273f4f44c8f6
Vladimir_Antin <antin502@gmail.com>
2018-10-08 21:03:49 +0200

    contactme test

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatIconModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatInputModule} from '@angular/material';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from '../home/home.component';
 import {ContactmeComponent} from '../contactme/contactme.component';
@@ -15,9 +15,9 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule, MatIconModule, MatButtonModule, RouterModule.forRoot(appRoutes),
+    CommonModule, MatIconModule, MatButtonModule, MatInputModule, RouterModule.forRoot(appRoutes),
   ],
   declarations: [],
-  exports: [MatIconModule, MatButtonModule, RouterModule]
+  exports: [MatIconModule, MatButtonModule, RouterModule, MatInputModule]
 })
 export class SharedModule { }
