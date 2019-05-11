4eddf0328811672a932d30b104587930795b4207
Vladimir_Antin <antin502@gmail.com>
2019-05-11 15:06:32 +0200

    remove twitter, add new font

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   actions: {action, class: string, name: string}[] = [
-    {action: () => this.goTo('https://facebook.com/100000213469440'), class: 'fab fa-facebook-square', name: 'Facebook'},
-    {action: () => this.goTo('https://twitter.com/propali_umetnik'), class: 'fab fa-twitter-square', name: 'Twitter'},
-    {action: () => this.goTo('https://www.linkedin.com/in/vladimir-antin'), class: 'fab fa-linkedin', name: 'LinkedIn'},
-    {action: () => this.goTo('https://www.instagram.com/kami.1196'), class: 'fab fa-instagram', name: 'Instagram'},
+    { action: () => this.goTo('https://facebook.com/100000213469440'), class: 'fab fa-facebook-square', name: 'Антин Владимир'},
+    { action: () => this.goTo('https://www.linkedin.com/in/vladimir-antin'), class: 'fab fa-linkedin', name: 'Vladimir Antin'},
+    { action: () => this.goTo('https://www.instagram.com/kami.1196'), class: 'fab fa-instagram', name: 'kami.1196'},
   ];
 
   constructor() { }
