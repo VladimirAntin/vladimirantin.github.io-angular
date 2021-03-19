b47802f2181a520d8ff080a57f291683c7e99ad3
Vladimir Antin <antin502@gmail.com>
2021-03-19 15:34:41 +0100

    ig acc

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,15 +7,18 @@ import {openExternalLink} from '../shared/variables';
 })
 export class FooterComponent implements OnInit {
 
+  username = 'vladimir.antin';
+
   actions: {action: () => any, class: string, name: string}[] = [
     { action: () => openExternalLink('https://facebook.com/100000213469440'), class: 'fab fa-facebook-square', name: 'Антин Владимир'},
     { action: () => openExternalLink('https://www.linkedin.com/in/vladimir-antin'), class: 'fab fa-linkedin', name: 'Vladimir Antin'},
-    { action: () => openExternalLink('https://www.instagram.com/vladimir.antin'), class: 'fab fa-instagram', name: 'vladimir.antin'},
+    { action: () => openExternalLink(`https://www.instagram.com/${this.username}`), class: 'fab fa-instagram', name: this.username}
   ];
   currentYear = new Date();
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
