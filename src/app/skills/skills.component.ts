410bdf7b143d31dc9b701fdccc3d4ecf8f98647d
Vladimir Antin <antin502@gmail.com>
2022-06-12 20:52:47 +0200

    skills, project - multiselect

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,40 +10,40 @@ export class SkillsComponent implements OnInit {
     {
       name: 'Programming languages',
       icon: 'icon-languages',
-      pattern: ['Java', 'JavaScript', 'TypeScript', 'HTML', 'CSS (Scss, less)', 'JSON', 'Basics of SQL', 'Basics of NoSQL'],
+      pattern: ['Java', 'JavaScript', 'PHP', 'TypeScript', 'HTML', 'CSS (Scss, less)', 'JSON', 'Basics of SQL', 'Basics of NoSQL'],
       spans: this.generateArray(39)
     },
     {
       name: 'Frameworks and libraries',
       icon: 'icon-framework',
-      pattern: ['Spring boot', 'Angular', 'NestJS', 'React', 'React native'],
+      pattern: ['Spring boot', 'Symfony', 'Angular', 'NestJS', 'NextJS', 'React', 'React native'],
       spans: this.generateArray(21)
     },
     {name: 'Database', icon: 'icon-db', pattern: ['MongoDB', 'MySQL'], spans: this.generateArray(13)},
     {
       name: 'Project Management',
       icon: 'icon-project-management',
-      pattern: ['maven', 'npm'],
+      pattern: ['maven', 'npm', 'composer'],
       spans: this.generateArray(34)
     },
     {
       name: 'IDE',
       icon: 'icon-ide',
-      pattern: ['IntelliJ IDEA', 'WebStorm', 'Visual Studio Code', 'Eclipse', 'Sublime Text'],
+      pattern: ['IntelliJ IDEA', 'WebStorm', 'PhpStorm', 'PyCharm', 'Visual Studio Code', 'Sublime Text'],
       spans: this.generateArray(39)
     },
     {name: 'Systems', icon: 'icon-os', pattern: ['Mac OSX', 'Linux', 'Windows'], spans: this.generateArray(40)},
-    {name: 'Version Control', icon: 'icon-git', pattern: ['Git']},
+    {name: 'Version Control', icon: 'icon-git', pattern: ['Git', 'Bitbucket']},
     {
       name: 'Other',
       icon: 'icon-other',
-      pattern: ['WebSocket', 'REST', 'JWT', 'Bootstrap', 'Material design', 'Fontawesome icons', 'Material icons'],
+      pattern: ['WebSocket', 'REST', 'JWT','Firebase', 'Bootstrap', 'Material design', 'Fontawesome icons', 'Material icons'],
       spans: this.generateArray(17)
     },
     {
       name: 'I am currently learning',
       icon: 'icon-learn',
-      pattern: ['oAuth', 'GraphQL', 'Electron', 'Docker'],
+      pattern: ['oAuth', 'GraphQL', 'Electron', 'Photoshop', 'Illustrator', 'Python (Flask)', 'Docker', 'Web3'],
       spans: this.generateArray(34)
     },
   ];
